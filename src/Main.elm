bc56db9d9a42d52e2a186f081b5def725264dcb2
Bede Kelly <bedekelly97@gmail.com>
2017-08-01 15:25:38 +0100

    11: Calling functions

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,23 @@
 module Main exposing (..)
 
-import Html exposing (li, ol, text)
+import Html exposing (text)
+
+
+four : Float
+four =
+    sqrt 16
+
+
+eleven : Float
+eleven =
+    max 2 11
+
+
+twenty : Float
+twenty =
+    max (sqrt 100) (4 * 5)
 
 
 main : Html.Html msg
 main =
-    viewStrings
-        [ "Hello"
-        , "ho" ++ "la"
-        , String.reverse "stressed"
-        , String.right 4 "foxglove"
-        , String.left 20 multilineString
-        ]
-
-
-multilineString : String
-multilineString =
-    """
-All happy families are alike; each unhappy family is unhappy in its own way.
-
-All was confusion in the Oblonskys' house...
-"""
-
-
-viewStrings : List String -> Html.Html msg
-viewStrings strings =
-    ol [] (List.map viewString strings)
-
-
-viewString : String -> Html.Html msg
-viewString string =
-    li [] [ text string ]
+    text (toString [ four, eleven, twenty ])
