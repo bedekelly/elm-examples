8ae3af11a0d30cdfdf3b2be9f55fca2ba54c1ce8
Bede Kelly <bedekelly97@gmail.com>
2017-08-01 16:54:46 +0100

    19: Random Numbers

--- src/Main.elm
+++ src/Main.elm
@@ -1,18 +1,52 @@
 module Main exposing (..)
 
-import Html exposing (text, Html)
+import Html exposing (div, h1, button, text)
+import Html.Events exposing (onClick)
+import Random
 
 
-isNear : Int -> Int -> Bool
-isNear x y =
-    abs (x - y) < 10
+type alias Model =
+    { dieFace : Int }
 
 
-increment : Int -> Int
-increment x =
-    x + 1
+type Msg
+    = Roll
+    | NewFace Int
 
 
-main : Html msg
+view : Model -> Html.Html Msg
+view model =
+    div []
+        [ h1 [] [ text (toString model.dieFace) ]
+        , button [ onClick Roll ] [ text "Roll" ]
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Roll ->
+            ( model, Random.generate NewFace (Random.int 1 6) )
+
+        NewFace newFace ->
+            ( Model newFace, Cmd.none )
+
+
+init : ( Model, Cmd msg )
+init =
+    ( Model 1, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
 main =
-    text (toString (isNear 72 (increment 75)))
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
