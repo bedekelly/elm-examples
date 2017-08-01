66dbec9528c7b78eef33c3490349fa8983621944
Bede Kelly <bedekelly97@gmail.com>
2017-08-01 11:15:04 +0100

    2: Unordered List

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,18 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, li, ul)
+import Html.Attributes exposing (class)
 
 
 main : Html.Html msg
 main =
-    text "Hello, world!"
+    ul [ class "shopping-list" ]
+        [ li [] [ text "Pamplemoose" ]
+        , li [] [ text "Ananas" ]
+        , li [] [ text "Jus d'Orange" ]
+        , li [] [ text "Boeuf" ]
+        , li [] [ text "Soup du Jour" ]
+        , li [] [ text "Camembert" ]
+        , li [] [ text "Jacques Cousteau" ]
+        , li [] [ text "Baguette" ]
+        ]
