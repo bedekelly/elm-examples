d435203c352f7b1e7a39e36a825dc91e45a3a94a
Bede Kelly <bedekelly97@gmail.com>
2017-08-03 11:38:25 +0100

    22: Webhooks

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
-import Time exposing (Time, second)
+import Html exposing (..)
+import Html.Events exposing (..)
+import WebSocket
 
 
 main : Program Never Model Msg
@@ -21,12 +20,14 @@ main =
 
 
 type alias Model =
-    Time
+    { input : String
+    , messages : List String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( Model "" [], Cmd.none )
 
 
 
@@ -34,34 +35,46 @@ init =
 
 
 type Msg
-    = Tick Time
+    = Input String
+    | Send
+    | NewMessage String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg { input, messages } =
     case msg of
-        Tick newTime ->
-            ( newTime, Cmd.none )
+        Input newInput ->
+            ( Model newInput messages, Cmd.none )
+
+        Send ->
+            ( Model "" messages, WebSocket.send "ws://echo.websocket.org" input )
+
+        NewMessage str ->
+            ( Model input (str :: messages), Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Time.every second Tick
+    WebSocket.listen "ws://echo.websocket.org" NewMessage
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    let
-        angle =
-            turns (Time.inMinutes model)
+    div []
+        [ div [] (List.map viewMessage model.messages)
+        , input [ onInput Input ] []
+        , button [ onClick Send ] [ text "Send" ]
+        ]
 
-        handX =
-            toString (50 + 40 * cos angle)
 
-        handY =
-            toString (50 + 40 * sin angle)
-    in
-        svg [ viewBox "0 0 100 100", width "300px" ]
-            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-            ]
+viewMessage : String -> Html Msg
+viewMessage msg =
+    div [] [ text msg ]
